8262aea24bd1b59849b94231849350eab329475d
Cesar Nasciment <ceesar90@gmail.com>
2021-05-02 14:30:09 -0300

    updating pizzas images

--- src/data/pizzas.js
+++ src/data/pizzas.js
@@ -3,21 +3,19 @@ export const sizes = [
     title: 'Pequena',
     description: 'Massa pequena ideal para 1 pessoa',
     price: 5,
-    image:
-      'https://thumbs.dreamstime.com/z/small-size-pizza-chicken-cheese-black-olives-food-small-size-pizza-chicken-cheese-black-olives-166346064.jpg',
+    image: 'https://i.imgur.com/REcYtUX.png',
   },
   {
     title: 'Média',
     description: 'Massa média ideal para 2 ou 3 pessoas',
     price: 10,
-    image:
-      'https://media.istockphoto.com/photos/tasty-pepperoni-pizza-and-cooking-ingredients-tomatoes-basil-on-black-picture-id1083487948?k=6&m=1083487948&s=612x612&w=0&h=lK-mtDHXA4aQecZlU-KJuAlN9Yjgn3vmV2zz5MMN7e4=',
+    image: 'https://i.imgur.com/S8parrg.png',
   },
   {
     title: 'Grande',
     description: 'Massa Grande ideal para 3 ou 4 pessoas',
     price: 15,
-    image: 'https://chicoterra.com/wp-content/uploads/2020/07/pizza.jpg',
+    image: 'https://i.imgur.com/yApyrDr.png',
   },
 ];
 export const doughs = [
@@ -26,24 +24,21 @@ export const doughs = [
     description:
       'Massa fina e crocante,assada uniformemente, que realça de maneira única os ingredientes da pizza.',
     price: 8,
-    image:
-      'https://images-gmi-pmc.edge-generalmills.com/4cb4c043-e72c-49cf-ba90-99bc4e76cd7b.jpg',
+    image: 'https://i.imgur.com/8vNo6N5.png',
   },
   {
     title: 'Massa média',
     description:
       'Massa tradicional . Com uma borda generosa e macia. Inspirada na clássica receita italiana.',
     price: 12,
-    image:
-      'https://bellyfull.net/wp-content/uploads/2016/01/Easy-Pizza-Dough-blog.jpg',
+    image: 'https://i.imgur.com/nVhjVyI.png',
   },
   {
     title: 'Massa grande',
     description:
       'A massa mais famosa da StoomPizza. Ela é alta, dourada, crocante por fora e macia por dentro, como só a StoomPizza sabe fazer.',
     price: 16,
-    image:
-      'https://3f4c2184e060ce99111b-f8c0985c8cb63a71df5cb7fd729edcab.ssl.cf2.rackcdn.com/media/16868/one-hourpizzadough.jpg',
+    image: 'https://i.imgur.com/eV0L69B.png',
   },
 ];
 export const border = [
@@ -51,22 +46,19 @@ export const border = [
     title: 'Borda de catupiry',
     description: 'Deliciosa borda com muito catupiry.',
     price: 5,
-    image:
-      'https://t2.rg.ltmcdn.com/pt/images/1/4/2/img_pizza_com_borda_recheada_4241_orig.jpg',
+    image: 'https://i.imgur.com/bKsowJB.png',
   },
   {
     title: 'Borda de cheddar',
     description: 'Deliciosa borda com muito cheddar.',
     price: 5,
-    image:
-      'https://media-cdn.tripadvisor.com/media/photo-s/12/44/1c/75/borda-recheada-de-cheddar.jpg',
+    image: 'https://i.imgur.com/s7NeOMd.png',
   },
   {
     title: 'Borda de bacon',
     description: 'Deliciosa borda com muito bacon.',
     price: 5,
-    image:
-      'https://www.tasteandtellblog.com/wp-content/uploads/2020/01/Chicken-Bacon-Ranch-Pizza-1.jpg',
+    image: 'https://i.imgur.com/MHyET8s.png',
   },
 ];
 export const pizzas = [
@@ -76,8 +68,7 @@ export const pizzas = [
     border: 'Cheddar',
     price: 40,
     points: 30,
-    image:
-      'https://blog.praticabr.com/wp-content/uploads/2019/06/Pizza-Pizzaria-Forno-Forza-Express.jpg',
+    image: 'https://i.imgur.com/f4UgD4J.png',
   },
   {
     size: 'Média',
@@ -85,8 +76,7 @@ export const pizzas = [
     border: 'Cheddar e catupiry',
     price: 55,
     points: 70,
-    image:
-      'https://www.alegrafoods.com.br/wp-content/uploads/2020/07/9-img-blog.png',
+    image: 'https://i.imgur.com/a7U3p2z.png',
   },
   {
     size: 'Grande',
@@ -94,7 +84,7 @@ export const pizzas = [
     border: 'Cheddar e bacon',
     price: 60,
     points: 75,
-    image: 'https://astron.com.br/wp-content/uploads/2017/07/pizza-site-or.jpg',
+    image: 'https://i.imgur.com/u6O1NNZ.png',
   },
   {
     size: 'Grande',
@@ -102,8 +92,7 @@ export const pizzas = [
     border: 'Cheddar, bacon e catupiry',
     price: 70,
     points: 105,
-    image:
-      'https://media-cdn.tripadvisor.com/media/photo-s/18/1a/d5/1e/casteloes.jpg',
+    image: 'https://i.imgur.com/MHyET8s.png',
   },
   {
     size: 'Média',
@@ -111,8 +100,7 @@ export const pizzas = [
     border: 'Bacon',
     price: 40,
     points: 69,
-    image:
-      'https://img.estadao.com.br/thumbs/640/resources/jpg/8/9/1569458498298.jpg',
+    image: 'https://i.imgur.com/bKsowJB.png',
   },
   {
     size: 'Pequena',
@@ -120,7 +108,6 @@ export const pizzas = [
     border: 'Bacon e catupiry',
     price: 70,
     points: 99,
-    image:
-      'https://res.cloudinary.com/mrancho/c_lfill,h_780,w_1200,g_center,ar_1.9,q_70/cardapio/2016/06/pizza-napolitana_min.jpg',
+    image: 'https://i.imgur.com/a7U3p2z.png',
   },
 ];
